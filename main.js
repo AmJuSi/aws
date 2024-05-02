80d6775b2803ba6f553358fdb0fc66ccdd36fa5a
AmJuSi <Amelie.Singler@student.uibk.ac.at>
2024-05-02 17:37:50 +0200

    wind

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ let map = L.map("map", {
 // thematische Layer
 let themaLayer = {
     stations: L.featureGroup(),
-    temperature: L.featureGroup().addTo(map),
+    temperature: L.featureGroup(),
+    wind: L.featureGroup().addTo(map),
 }
 
 // Hintergrundlayer
@@ -28,7 +29,8 @@ L.control.layers({
     "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery")
 }, {
     "Wetterstationen": themaLayer.stations,
-    "Temperatur": themaLayer.temperature,
+    "Temperatur °C": themaLayer.temperature,
+    "Wind km/h": themaLayer.wind,
 }).addTo(map);
 
 // Maßstab
@@ -69,6 +71,29 @@ function showTemperature(geojson) {
     }).addTo(themaLayer.temperature);
 }
 
+//funktion wind definieren
+function showWind(geojson) {
+    L.geoJSON(geojson, {
+        filter: function (feature) {
+            //feature.properties.WG
+            if (feature.properties.WG > -5 && feature.properties.WG < 1000) {
+                return true;
+            }
+        },
+        pointToLayer: function (feature, latlng) {
+            let color = getColor(feature.properties.WG, COLORS.wind);
+            return L.marker(latlng, {
+                icon: L.divIcon({
+                    className: "aws-div-icon",
+                    html: `<span style="background-color:${color};">${feature.properties.WG.toFixed(1)}</span>`
+                })
+            })
+        }
+    }).addTo(themaLayer.wind);
+}
+
+
+
 // GeoJSON der Wetterstationen laden
 async function showStations(url) {
     let response = await fetch(url);
@@ -104,6 +129,7 @@ async function showStations(url) {
     }).addTo(themaLayer.stations);
 
     showTemperature(geojson);
+    showWind(geojson);
 
 }
 showStations("https://static.avalanche.report/weather_stations/stations.geojson");
