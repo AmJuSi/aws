4f6466de1028622472add627cb3c85a7614b374d
AmJuSi <Amelie.Singler@student.uibk.ac.at>
2024-05-02 16:31:13 +0200

    filter und runden von nachkommastellen

--- main.js
+++ main.js
@@ -39,11 +39,17 @@ L.control.scale({
 //funktion temperatur definieren
 function showTemperature(geojson) {
     L.geoJSON(geojson, {
+        filter: function (feature) {
+            //feature.properties.LT
+            if (feature.properties.LT > -50 && feature.properties.LT < 50) {
+                return true;
+            }
+        },
         pointToLayer: function (feature, latlng) {
             return L.marker(latlng, {
                 icon: L.divIcon({
                     className: "aws-div-icon",
-                    html: `<span>${feature.properties.LT}</span>`
+                    html: `<span>${feature.properties.LT.toFixed(1)}</span>`
                 })
             })
         }
